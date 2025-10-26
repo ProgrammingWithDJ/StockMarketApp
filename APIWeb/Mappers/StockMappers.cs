89d3e131258e6e47a12a87cd6a04acf7bcb67f8a
Dipesh Jadhav <dipxsss@gmail.com>
2025-10-26 20:04:12 +0530

    Add dto and mapper

---
+++ APIWeb/Mappers/StockMappers.cs
@@ -0,0 +1,21 @@
+﻿using APIWeb.Dtos.Stocks;
+
+namespace APIWeb.Mappers
+{
+    public static class StockMappers
+    {
+        public static StocksDto ToStockDto(this Stock stockModel)
+        {
+            return new StocksDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
