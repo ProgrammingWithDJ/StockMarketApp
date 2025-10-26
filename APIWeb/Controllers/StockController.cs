3cccd2f527846de698cd8058c589ee6f2cc70c5f
Dipesh <Dipesh@DESKTOP-VQS2QCC>
2025-10-26 19:18:58 +0530

    Add project files.

---
+++ APIWeb/Controllers/StockController.cs
@@ -0,0 +1,39 @@
+﻿using APIWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIWeb.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStocks()
+        {
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("Id")]
+        public IActionResult GetStock([FromRoute] int Id)
+        {
+            var stock = _context.Stock.Find(Id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+
+
+
+    }
+}
---
+++ APIWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
