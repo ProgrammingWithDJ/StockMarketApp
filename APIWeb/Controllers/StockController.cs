b7acf440a2a4ddd0c9a096f3e569951e9249586f
Dipesh Jadhav <dipxsss@gmail.com>
2025-11-02 19:46:44 +0530

    add update and delete method

--- APIWeb/Controllers/StockController.cs
+++ APIWeb/Controllers/StockController.cs
@@ -47,5 +47,39 @@ namespace APIWeb.Controllers
 
 
         }
+
+        [HttpPut]
+        [Route("{Id}")]
+        public IActionResult UpdateStock([FromRoute] int Id, [FromBody] UpdateStockDto stockDto)
+        {
+            var stockModel = _context.Stock.Find(Id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = stockDto.Symbol;
+            stockModel.CompanyName = stockDto.CompanyName;
+            stockModel.Purchase = stockDto.Purchase;
+            stockModel.LastDiv = stockDto.LastDiv;
+            stockModel.Industry = stockDto.Industry;
+            stockModel.MarketCap = stockDto.MarketCap;
+            _context.Stock.Update(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{Id}")]
+        public IActionResult DeleteStock([FromRoute] int Id)
+        {
+            var stockModel = _context.Stock.Find(Id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stock.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
